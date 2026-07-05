63d66967e96b137dbd73fe7a8873d814a155425f
deathmaskXE <deathmask2014@gmail.com>
2026-07-05 02:32:19 -0600

    Delete js directory

--- js/config.js
+++
@@ -1,9 +0,0 @@
-window.XE_CONFIG = {
-  businessName: "XE Servicio Electrónico",
-  whatsappNumber: "527471731852",
-  payments: {
-    bancoppel: { label:"BanCoppel", detail:"Transferencia bancaria o depósito en OXXO", holder:"Irving Daniel Sánchez", account:"4169 1606 3015 8435" },
-    banorte: { label:"Banorte", detail:"Transferencia bancaria o depósito en OXXO", holder:"Irving Daniel Sánchez", account:"4189 1400 1021 7767" },
-    mercadopago: { label:"Mercado Pago", detail:"Solo transferencia bancaria", holder:"Irving Daniel Sánchez Nava", account:"722969020089368829", accountLabel:"CLABE" }
-  }
-};
